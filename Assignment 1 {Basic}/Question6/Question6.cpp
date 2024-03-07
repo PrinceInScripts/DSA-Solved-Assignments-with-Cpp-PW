f2d9f8068f1d646fafd9cbfbea937e9e35f4b1c8
princekumar <princepk7878kumar@gmail.com>
2024-03-07 21:57:56 +0530

    done assignment1 question6

---
+++ Assignment 1 {Basic}/Question6/Question6.cpp
@@ -0,0 +1,26 @@
+/*
+Q6. How many of these can be a variable name ?
+a) 01Pwskills
+b) 0FLOAT
+c) int
+d) FLOAT
+e) You will succeed
+*/
+
+/*
+Ans .
+In their only have one variable name is syntactically correct, that's FLOAT.
+
+a) 01Pwskills
+It not a variable because in variable name start with digit and these is syntactically incorrect.
+
+b) 0FLOAT
+It not a variable because in variable name start with digit and these is syntactically incorrect.
+
+c) int
+It not a variable because in variable name is a keyword and these is syntactically incorrect.
+
+e) You will succeed
+It not a variable because in variable name have space between word and these is syntactically incorrect.
+
+*/
