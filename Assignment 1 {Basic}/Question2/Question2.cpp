64d2a0020fba8db3db34799295d24da4fb45c6a6
princekumar <princepk7878kumar@gmail.com>
2024-03-07 21:21:32 +0530

    done assignment1 question2

---
+++ Assignment 1 {Basic}/Question2/Question2.cpp
@@ -0,0 +1,12 @@
+// Q2. Write a program to print 10 using 2 positive numbers less than 6 in C++ as output.
+
+/*
+Ans. we takeing two variable a and b , the value of a and is 5. because is a positive number less then 6 and their sum is 10 
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5,b=5;
+    cout<<"the sum of two positive integer is : "<<a+b;
+}
