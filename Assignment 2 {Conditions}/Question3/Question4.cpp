d3a2c8108a258af5d1221e83abcae11110996c70
princekumar <princepk7878kumar@gmail.com>
2024-03-13 08:42:21 +0530

    done assignment 2 03 question

---
+++ Assignment 2 {Conditions}/Question3/Question4.cpp
@@ -0,0 +1,27 @@
+
+/*
+Q3. Find the output for this code. Let input:- 2 3
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int x,y;
+cin>>x>>y;
+x+=y;
+x-=y;
+x%=y;
+cout<<x;
+}
+*/
+
+/*
+#output
+
+2
+3
+2
+
+
+//2 and 3 is input and 2 is the output
+*/
