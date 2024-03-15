58bf5f12cb6075e875465e1c94d0ab64e579d2e4
princekumar <princepk7878kumar@gmail.com>
2024-03-15 16:58:40 +0530

    done Assignment 3 assignment 2 question1

---
+++ Assignment 3 {Loops}/Assignment 2 {Loops2}/Question 1/Question1.cpp
@@ -0,0 +1,17 @@
+/*
+Q1. Predict the output
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+while ('1' < '2')
+cout << "In while loop" << endl;
+}
+*/
+
+/*
+#output
+
+In while loop
+*/
+
