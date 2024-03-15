587e3b2c88e8168e2e233d60c120f017d1566f84
princekumar <princepk7878kumar@gmail.com>
2024-03-15 17:06:56 +0530

    done Assignment 3 assignment 2 question2

---
+++ Assignment 3 {Loops}/Assignment 2 {Loops2}/Question 2/Question2.cpp
@@ -0,0 +1,20 @@
+/*
+Q2. Predict the output
+
+#include <bits/stdc++.h>
+using namespace std;
+int main( ) {
+int t = 10;
+while (t /= 2) {
+cout << "Hello" << endl;
+}
+}
+*/
+
+
+/*
+#output
+Hello
+Hello
+Hello
+*/
