a6061d11f018acfbed837eb84fad166e4b88e73f
princekumar <princepk7878kumar@gmail.com>
2024-03-21 16:23:45 +0530

    Assignmnet 4 02 assignment Question1

---
+++ Assignment 4 {Pattern Printing}/Assignment Part 2/Question1/Question1.cpp
@@ -0,0 +1,28 @@
+/*
+Q1. Print the following pattern
+
+     1
+   1 2 3
+  1 2 3 4 5
+1 2 3 4 5 6 7
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+
+        for(int j=1;j<=2*i-1;j++){
+            cout<<j<<" ";
+        }
+
+        cout<<endl;
+    }
+}
