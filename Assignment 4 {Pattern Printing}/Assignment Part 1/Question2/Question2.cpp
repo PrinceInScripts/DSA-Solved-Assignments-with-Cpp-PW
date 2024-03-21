44a7aefd93046b3e492d9411863bb7e8261c53ea
princekumar <princepk7878kumar@gmail.com>
2024-03-21 15:26:11 +0530

    Assignmnet 4 01 assignment Question3

--- Assignment 4 {Pattern Printing}/Assignment Part 1/Question2/Question2.cpp
+++ Assignment 4 {Pattern Printing}/Assignment Part 1/Question2/Question2.cpp
@@ -2,10 +2,10 @@
 Q1. Print the following pattern
 Input: n = 4
 Output:
-1 2 3 4
-1 2 3
-1 2
-1
+A
+A B
+A B C
+A B C D
 */
 
 /*
@@ -20,8 +20,8 @@ int main(){
     cin>>n;
     
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i+1;j++){
-            cout<<j<<" ";
+        for(int j=1;j<=i;j++){
+            cout<<(char)(j+64)<<" ";
         }
         cout<<endl;
     }
