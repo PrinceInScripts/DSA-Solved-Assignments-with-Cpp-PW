2063effe9e831fbd5ac60ed2057f90d27493a1c7
princekumar <princepk7878kumar@gmail.com>
2024-03-21 15:21:58 +0530

    Assignmnet 4 01 assignment Question2

---
+++ Assignment 4 {Pattern Printing}/Assignment Part 1/Question2/Question2.cpp
@@ -0,0 +1,28 @@
+/*
+Q1. Print the following pattern
+Input: n = 4
+Output:
+1 2 3 4
+1 2 3
+1 2
+1
+*/
+
+/*
+Ans . 
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
