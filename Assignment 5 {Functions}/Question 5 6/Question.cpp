b6dac6ef65552e241cab58d68de84f8284e37ddd
princekumar <princepk7878kumar@gmail.com>
2024-03-24 11:40:16 +0530

    done assignment 5 question 5 6 7

---
+++ Assignment 5 {Functions}/Question 5 6/Question.cpp
@@ -0,0 +1,21 @@
+/*
+Q5. The minimum number of functions present in any C++ program is:
+Ans. Infinite
+*/
+
+
+
+/*
+Q6: State True and False:
+1. A function may be called more than once from any other functions  [true]
+Ôç It is necessary for a function to return some value.  [false]
+
+*/
+
+
+/*
+
+Q7: Explore:
+Can the same function name be used for different functions without any conflict?
+NO
+*/
