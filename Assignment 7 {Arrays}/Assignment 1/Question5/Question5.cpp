453472a6bd78408ecd74ae6da28d3cd11fa7a210
princekumar <princepk7878kumar@gmail.com>
2024-04-03 10:40:30 +0530

    Assignmnet 7 array assignnet 1 Question5

--- Assignment 7 {Arrays}/Assignment 1/Question5/Question5.cpp
+++ Assignment 7 {Arrays}/Assignment 1/Question5/Question5.cpp
@@ -2,3 +2,23 @@
 Q5. WAP to find the smallest missing positive element in the sorted Array that contains only
 positive elements.
 */
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,5,6};
+
+    int x=1;
+    int result;
+
+    for(int i=0;i<5;i++){
+        if(x==arr[i]){
+            x++;
+        } else if(arr[i]>x){
+             break;
+        }
+    }
+
+    cout<<"Missing positive element in the sorted Array is : "<<x;
+
+    
+}
