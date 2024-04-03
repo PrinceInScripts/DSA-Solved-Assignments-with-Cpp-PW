21d5ebcd42f724f668ef64d3e76e7236934e7830
princekumar <princepk7878kumar@gmail.com>
2024-04-03 10:45:58 +0530

    Assignmnet 7 array assignnet 1 Question6

---
+++ Assignment 7 {Arrays}/Assignment 1/Question6/Question6.cpp
@@ -0,0 +1,21 @@
+/*
+Q6: Predict the output.
+
+int main()
+{
+int sub[50], i ;
+for ( i = 0 ; i <= 48 ; i++ ) ;
+{
+sub[i] = i ;
+cout<<sub[i]<<endl ;
+}
+return 0;
+}
+*/
+
+
+/*
+#output
+49
+*/
+
