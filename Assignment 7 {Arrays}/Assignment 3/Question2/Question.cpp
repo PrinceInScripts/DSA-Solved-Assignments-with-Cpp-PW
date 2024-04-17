d69c8ab479d5e2c51c3a90b5e8c0b7759985b310
princekumar <princepk7878kumar@gmail.com>
2024-04-17 11:35:54 +0530

    done assignment 7 array assignment 3 Question 2

---
+++ Assignment 7 {Arrays}/Assignment 3/Question2/Question.cpp
@@ -0,0 +1,66 @@
+/*
+Q2. Find the factorial of a large number.
+*/
+
+#include<iostream>
+using namespace std;
+int mul(int x,int res[],int res_size){
+    int carry=0;
+
+    for(int i=0;i<res_size;i++){
+        int prod=res[i]*x+carry;
+        res[i]=prod%10;
+        carry=prod/10;;
+    }  
+
+    while(carry){
+        res[res_size]=carry%10;
+        carry=carry/10;
+        res_size++;
+    }
+
+    return res_size;
+}
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    
+    int res[500];
+    res[0]=1;
+    int res_size=1;
+
+    for(int i=2;i<=n;i++){
+        res_size=mul(i,res,res_size);
+    }
+
+    for(int i=res_size-1;i>=0;i--){
+        cout<<res[i];
+    }
+    
+    
+}
+
+// #include <iostream>
+// #include <boost/multiprecision/cpp_int.hpp>
+
+// using namespace std;
+// using namespace boost::multiprecision;
+
+// cpp_int fact(int num) {
+//     cpp_int result = 1;
+//     for (int i = 1; i <= num; i++) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// int main() {
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+
+//     cpp_int f = fact(n);
+//     cout << f << endl;
+//     return 0;
+// }
