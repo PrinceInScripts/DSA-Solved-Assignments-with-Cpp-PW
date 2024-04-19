62998f3e4a9da227bece685a2aaf6f973df418f6
princekumar <princepk7878kumar@gmail.com>
2024-04-19 21:36:34 +0530

    done

---
+++ Assignment 8 {2D Arrays}/Assignment 1/Question2/Question2.cpp
@@ -0,0 +1,11 @@
+/*
+Q2. Given a matrix ‘A’ of dimension n x m and 2 coordinates (l1, r1) and (l2, r2). Return the sum of the
+rectangle from (l1,r1) to (l2, r2).
+
+Input 1:
+1 2 -3 4
+0 0 -4 2
+1 -1 2 3
+-4 -5 -7 0
+l1 = 1, r1 = 2 , l2 = 3 , r2 = 3
+*/
