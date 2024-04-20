322f96aff7a629654ec12519a3eae2004f695479
princekumar <princepk7878kumar@gmail.com>
2024-04-20 21:55:13 +0530

    done question1

--- Assignment 8 {2D Arrays}/Assignment 1/Question2/Question2.cpp
+++ Assignment 8 {2D Arrays}/Assignment 1/Question2/Question2.cpp
@@ -9,3 +9,34 @@ Input 1:
 -4 -5 -7 0
 l1 = 1, r1 = 2 , l2 = 3 , r2 = 3
 */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<vector<int>> v={{1,2,-3,4},{0,0,-4,2},{1,-1,2,3},{-4,-5,-7,0}};
+    int l1,l2,r1,r2;
+    cout<<"Enter a coordinate : ";
+    cin>>l1>>r1;
+    cout<<"Enter Another coordinate : ";
+    cin>>l2>>r2;
+    int sum=0;
+    
+    if(l2>l1){
+        for(int i=l1;i<=l2;i++){
+        for(int j=r1;j<=r2;j++){
+               sum+=v[i][j];
+        }
+    }
+    } else {
+    for(int i=l2;i<=l1;i++){
+        for(int j=r1;j<=r2;j++){
+               sum+=v[i][j];
+        }
+    }
+    }
+
+    cout<<sum;
+   
+    
+}
