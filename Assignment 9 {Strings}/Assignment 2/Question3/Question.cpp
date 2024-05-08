766dcd49f1d5bb2aa0a0741dd6cc8cea7e181557
princekumar <princepk7878kumar@gmail.com>
2024-05-08 22:25:36 +0530

    done

--- Assignment 9 {Strings}/Assignment 2/Question3/Question.cpp
+++ Assignment 9 {Strings}/Assignment 2/Question3/Question.cpp
@@ -17,9 +17,16 @@ Output : 0
 using namespace std;
 int main(){
     string str;
-    cout<<"Enter a String";
+    cout<<"Enter a String : ";
     cin>>str;
-    
+    int count=0;
+    for(int i=0;i<str.size();i++){
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||  str[i]=='o' || str[i]=='u'  ){
+             count++;
+        }
+    }
+
+    cout<<count;
 }
 
 
