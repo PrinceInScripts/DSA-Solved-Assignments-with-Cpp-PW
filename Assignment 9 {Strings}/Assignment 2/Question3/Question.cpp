c62c163974cd64a120168d8ed42fb0b7ffa2dc2a
princekumar <princepk7878kumar@gmail.com>
2024-05-08 21:31:15 +0530

    done

---
+++ Assignment 9 {Strings}/Assignment 2/Question3/Question.cpp
@@ -0,0 +1,25 @@
+/*
+Q3. Input a string and return the number of substrings that contain only vowels
+Input : str = "abjkoe"
+
+Output : 4
+
+Explanation : The possible substrings that only contain vowels are "a" , "o" , "e" , "oe"
+
+Input : str = "hgdhpw"
+
+Output : 0
+*/
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a String";
+    cin>>str;
+    
+}
+
+
