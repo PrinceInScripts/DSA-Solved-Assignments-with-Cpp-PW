40a61fb9df7289de70c2c1f925dcc14529539531
princekumar <princepk7878kumar@gmail.com>
2024-05-27 15:28:45 +0530

    done

---
+++ Assignment 10 {Time and space  complexity}/Assignment 1/Question 1/Question.cpp
@@ -0,0 +1,28 @@
+/*
+Q1. Calculate the time complexity for the following code snippet.
+int c = 0;
+for(int i = n; i > 0; i /= 2) {
+c++;
+}
+*/
+/*
+Ans. 
+Time complexity : log(n)
+The iteration are
+1  n
+2  n/2
+3  n/4
+4  n/8
+5  n/16
+  .
+  .
+  .
+  .
+  .
+  .
+n  n/2^x
+
+n/2^x>0
+n/2^x=1
+x=log(n)
+*/
