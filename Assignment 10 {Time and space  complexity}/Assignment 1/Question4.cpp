30153392b91d653b8782b7cb09499308ae265330
princekumar <princepk7878kumar@gmail.com>
2024-05-27 15:45:13 +0530

    done

---
+++ Assignment 10 {Time and space  complexity}/Assignment 1/Question4.cpp
@@ -0,0 +1,29 @@
+/*
+Q2. Calculate the time complexity for the following code snippet.Q4. Calculate the time complexity for the following code snippet.
+int c = 0;
+for(int i = 1; i < n; i *= 2) {
+c++;
+}
+*/
+
+/*
+Ans. 
+Time complexity : O(log(n))
+The iteration are
+1  1
+2  1*2=2
+3  2*2=4
+4  4*2=8
+  .
+  .
+  .
+  .
+  .
+x  2^x
+
+2^x<n
+2^x=n
+x=log(n)
+
+because is iterate is 1 because he divided by n to n 
+*/
