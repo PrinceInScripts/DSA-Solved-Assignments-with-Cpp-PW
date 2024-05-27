89a9606134428869f9b8c1f44131a8f9f7ccb90d
princekumar <princepk7878kumar@gmail.com>
2024-05-27 15:47:14 +0530

    done

---
+++ Assignment 10 {Time and space  complexity}/Assignment 1/Question5.cpp
@@ -0,0 +1,13 @@
+/*
+Q5. Calculate the time complexity for the following code snippet.int c = 0;
+int c = 0;
+for(int i = 0; i < n; i++) {
+c +=i;
+}
+*/
+
+/*
+Ans. 
+Time complexity : O(n)
+nI these loop only adding on i on c that's not changes on iterate so the iteration is n
+*/
