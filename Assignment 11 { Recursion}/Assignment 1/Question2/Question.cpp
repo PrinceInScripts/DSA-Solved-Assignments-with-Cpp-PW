c2a4f0179a71d738a52c0de2bd7360b50b169a81
princekumar <princepk7878kumar@gmail.com>
2024-05-29 15:22:19 +0530

    done

--- Assignment 11 { Recursion}/Assignment 1/Question2/Question.cpp
+++ Assignment 11 { Recursion}/Assignment 1/Question2/Question.cpp
@@ -12,9 +12,16 @@ int stair(int n){
 
    return stair(n-1)+stair(n-2)+stair(n-3);
 }
+int stair1(int n){
+  if(n<0) return 0;
+   if(n==0) return 1;
+
+   return stair1(n-1)+stair1(n-2)+stair1(n-3);
+}
 int main(){
     int n;
     cout<<"Enter Steps of Stair : ";
     cin>>n;
-    cout<<"Ways of Climbs Steps : "<<stair(n);
+    cout<<"Ways of Climbs Steps : "<<stair(n)<<endl;
+    cout<<"Ways of Climbs Steps : "<<stair1(n);
 }
