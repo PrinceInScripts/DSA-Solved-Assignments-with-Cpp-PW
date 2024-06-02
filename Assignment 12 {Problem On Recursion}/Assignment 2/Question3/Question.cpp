0b6564df7dbd2b36e8e0c57cc68d4122eb9b7831
princekumar <princepk7878kumar@gmail.com>
2024-06-02 21:36:08 +0530

    done

---
+++ Assignment 12 {Problem On Recursion}/Assignment 2/Question3/Question.cpp
@@ -0,0 +1,15 @@
+/*
+Q3. Given a string, find the length of the longest common substring from two given strings.
+*/
+
+#include<iostream>
+using namespace std;
+int LongestSubstring(string str1,string str2){
+
+}
+int main(){
+    string str1="abcdef";
+    string str2="zcdemf";
+
+    cout<<LongestSubstring(str1,str2);
+}
