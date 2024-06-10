0ac3958b3e3768875d20e64dfb76e284f85a3e8d
princekumar <princepk7878kumar@gmail.com>
2024-06-10 15:24:05 +0530

    done

---
+++ Assignment 13 {Binary Search}/Assignment 1/Question2/Question.cpp
@@ -0,0 +1,29 @@
+/*
+Q2. Given a sorted binary array, efficiently count the total number of 1’s in it.
+Input 1 : a = [0,0,0,0,1,1]
+Output 1: 2
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a[]={0,0,0,0,1,1};
+
+    int n=sizeof(a)/sizeof(a[0]);
+    int lo=0;
+    int hi=n-1;
+    int count=0;
+
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+
+        if(a[mid]==1){
+            count++;
+            lo=mid+1;
+        } else {
+            lo=mid+1;
+        }
+    }
+    cout<<count;
+
+}
