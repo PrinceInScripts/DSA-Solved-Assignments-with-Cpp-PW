b3690fcb13b54f0a8f5ab1044b358f6d7ed4ccc6
princekumar <princepk7878kumar@gmail.com>
2024-06-10 16:17:15 +0530

    done

--- Assignment 13 {Binary Search}/Assignment 1/Question2/Question.cpp
+++ Assignment 13 {Binary Search}/Assignment 1/Question2/Question.cpp
@@ -7,23 +7,34 @@ Output 1: 2
 #include<iostream>
 using namespace std;
 int main(){
-    int a[]={0,0,0,0,1,1};
+    int a[]={0,0,1,1,1,1};
 
     int n=sizeof(a)/sizeof(a[0]);
     int lo=0;
     int hi=n-1;
     int count=0;
+    int x=-1;
 
     while(lo<=hi){
         int mid=lo+(hi-lo)/2;
 
         if(a[mid]==1){
-            count++;
-            lo=mid+1;
+            x=mid;
+            hi=mid-1;
         } else {
             lo=mid+1;
         }
     }
-    cout<<count;
+    // while(lo<=hi){
+    //     int mid=lo+(hi-lo)/2;
+
+    //     if(a[mid]==1){
+    //         count++;
+    //         lo=mid+1;
+    //     } else {
+    //         lo=mid+1;
+    //     }
+    // }
+    cout<<n-x;
 
 }
---
+++ Assignment 13 {Binary Search}/Assignment 1/Question4/Question.cpp
@@ -0,0 +1,31 @@
+/*
+Q4. Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n]
+inclusive in sorted order.
+There is only one repeated number in nums, return this repeated number.
+
+Input 1: arr[] = {1,2,3,3,4}
+Output 1: 3
+Input 2: arr[] = {1,2,2,3,4,5}
+Output 2: 2
+*/
+
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,3,4};
+    
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int lo=0;
+    int hi=n-1;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+
+        if(arr[mid]==arr[mid+1] || arr[mid]==arr[mid-1]){
+            cout<<arr[mid]<<endl;
+            break;
+        }
+    }
+    
+
+}
