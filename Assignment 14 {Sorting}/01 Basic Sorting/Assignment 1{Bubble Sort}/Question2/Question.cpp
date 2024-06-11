6ee1888e0e94c0fb14fae563355a06f9254b4253
princekumar <princepk7878kumar@gmail.com>
2024-06-11 21:24:34 +0530

    done

---
+++ Assignment 14 {Sorting}/01 Basic Sorting/Assignment 1{Bubble Sort}/Question2/Question.cpp
@@ -0,0 +1,21 @@
+/*
+Q5. Check if the given array is almost sorted. (elements are at-most one position away).
+*/
+
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+bool checkSort(vector<int>& nums){
+      int n=nums.size();
+      bool flag=false;
+      for(int i=0;i<n-1;i++){
+        if(nums[i]<nums[i+1]) flag=true;
+      }
+      return flag;
+}
+int main(){
+    vector<int> nums={1,2,3,4,5};
+
+    checkSort(nums)?cout<<"YES":cout<<"NO";
+}
