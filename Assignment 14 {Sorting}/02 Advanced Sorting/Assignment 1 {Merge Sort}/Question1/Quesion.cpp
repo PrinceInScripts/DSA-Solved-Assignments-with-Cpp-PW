7fe0844ebcb319ac4601b63d43659e06cfb39863
princekumar <princepk7878kumar@gmail.com>
2024-06-17 20:00:59 +0530

    done

--- Assignment 14 {Sorting}/02 Advanced Sorting/Assignment 1 {Merge Sort}/Question1/Quesion.cpp
+++ Assignment 14 {Sorting}/02 Advanced Sorting/Assignment 1 {Merge Sort}/Question1/Quesion.cpp
@@ -1,6 +1,5 @@
 /*
-Q3. Given an integer array and an integer k where k<=size of array, We need to return the kth
-smallest element of the array.
+Q1. Given an array of integers, sort it in descending order using merge sort algorithm.
 */
 
 #include<iostream>
@@ -9,26 +8,52 @@ smallest element of the array.
 #include<cmath>
 using namespace std;
 void merge(vector<int>& a,vector<int>& b,vector<int>& v){
-    int i=0,j=0,k=0;
-    while(i<a.size() && j<b.size()){
-          if(a[i]<b[j]) v[k++]=a[i++];
-          else v[k++]=b[j++];
+    int i=a.size()-1,j=b.size()-1,k=v.size()-1;
+    while(i>=0 && j>=0){
+          if(a[i]<b[j]) v[k--]=a[i--];
+          else v[k--]=b[j--];
     }
 
-    if(i==a.size())
-         while(j<b.size()) v[k++]=b[j++];
+    if(i==-1)
+         while(j>=0) v[k--]=b[j--];
     
-    if(j==b.size())
-             while(i<a.size()) v[k++]=a[i++];
+    if(j==-1)
+             while(i>=0) v[k--]=a[i--];
           
 }
+
+
+
+void mergeSort(vector<int>& v){
+    int n=v.size();
+    if(n==1) return;
+    int n1=n/2,n2=n-n/2;
+    vector<int> a(n1),b(n2);
+
+    for(int i=0;i<n1;i++){
+        a[i]=v[i];
+    }
+    for(int i=0;i<n2;i++){
+        b[i]=v[i+n1];
+    }
+
+    mergeSort(a);
+    mergeSort(b);
+
+    merge(a,b,v);
+}
+
 int main(){
-    vector<int> arr={3,4,7,10,15,20};
-    vector<int> arr1={10,20,30};
-    vector<int> result(arr.size()+arr1.size());
+    int arr[]={5,1,3,0,4,9,6};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr,arr+n);
+    for(int el:v){
+        cout<<el<<" ";
+    }
     
-    merge(arr,arr1,result);
-    for(int el:result){
+    mergeSort(v);
+    cout<<endl;
+    for(int el:v){
         cout<<el<<" ";
     }
 }
